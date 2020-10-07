36e8bdaeae55a4341e9f90e3667fbfd1ad04184a
Francesco <franesposito10@gmail.com>
2020-10-07 17:52:03 +0200

    Removal of a variable

--- Configuration_maker.py
+++ Configuration_maker.py
@@ -10,7 +10,7 @@ config['General_Variables'] = {
     'mixing_ratio' : '0',
     'wavelength_step' : '0.01',
     'angstrom_coefficient_alpha' : '1',
-    'BeightnessTemperature_of_the_sun' : '5800',
+    'BrightnessTemperature_of_the_sun' : '5800',
     'Vertical_scale_Height' : '7.7',
     'wavelength_norm' : '0.52',
     'distance_from_black_wall' :'25'
